79d68103211b0e4d44587f8c5d0b19a552cbfe58
root <root@centos1-k8s.homelab>
2019-04-09 17:25:12 +0300

    changed text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            !!!!! Hi hi hi Edit <code>src/App.js</code> and save to reload.
+            Weeeeee I've done something !!!!!
           </p>
           <a
             className="App-link"
