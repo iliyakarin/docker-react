324fbc0f78f1d9416e93dc1586337a991bb37545
root <root@centos-compose.local>
2020-12-10 22:07:21 +0300

    fix 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            "Weeeeee I've done something !!!" "Docker"
+            "I've done something !!!" "Docker"
           </p>
           <a
             className="App-link"
