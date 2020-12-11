2c2f211bc00adfdb242c36731d07cb4059a2e7b3
root <root@centos-compose.local>
2020-12-11 03:20:13 +0300

    Change message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>Hello Iliya!!!!!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
