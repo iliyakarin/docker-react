6c2f2426ebccb6bffd964c5749a7095dffeb34e3
root <root@centos-compose.local>
2020-12-17 12:50:26 +0300

    Change 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Iliya!!!!!!!</p>
+        <p>Hello Iliya!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
